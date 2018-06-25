90940ca02617d6e765299182c2be258152319c79
Sophie van Wersch <Sophie.van.Wersch-CIC.Netherlands@ibm.com>
2018-06-25 13:27:35 +0200

    outlets and actions added: data can be passed from VC to secondVC

--- PassingData/SecondViewController.swift
+++ PassingData/SecondViewController.swift
@@ -10,26 +10,21 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    var data = ""
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        label.text = data
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func sendDataBack(_ sender: Any) {
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- PassingData/ViewController.swift
+++ PassingData/ViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func sendButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "sendDataForwards", sender: self)
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "sendDataForwards" {
+            
+            let secondVC = segue.destination as! SecondViewController
+            
+            secondVC.data = textField.text!
+        }
+    }
 }
 
